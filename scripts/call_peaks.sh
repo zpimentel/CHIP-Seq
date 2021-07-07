3722ff2a2a13398901e6c803df3b65793dfd75f2
Zachary Pimentel <zpimentel@ssh3.hac.uri.edu>
2021-07-06 23:43:16 -0400

    Add genome version to call_peaks.sh

--- scripts/call_peaks.sh
+++ scripts/call_peaks.sh
@@ -17,7 +17,7 @@ module load epic/20181121-foss-2020a
 epic --treatment SRX017887.bed SRX017888.bed SRX017889.bed SRX017890.bed SRX017891.bed --control SRX017892.bed SRX017893.bed --genome hg38 --outfile ../../03_peak_calling/GM10847.out
 
 # Call peaks with epic for GM15510 cells
-epic --treatment SRX017875.bed SRX017876.bed SRX017877.bed SRX017878.bed SRX017879.bed --control SRX017881.bed SRX017880.bed --outfile ../../03_peak_calling/GM15510.out
+epic --treatment SRX017875.bed SRX017876.bed SRX017877.bed SRX017878.bed SRX017879.bed --control SRX017881.bed SRX017880.bed --genome hg38 --outfile ../../03_peak_calling/GM15510.out
 
 echo "DONE"
 date
