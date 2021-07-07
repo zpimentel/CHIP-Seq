b775b68133ff27484716eaf3e66d9f6a0de7f219
Zachary Pimentel <zpimentel@ssh3.hac.uri.edu>
2021-07-06 20:21:25 -0400

    Request exclusive use of node for indexing

--- scripts/index_ref_genome.sh
+++ scripts/index_ref_genome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
+#SBATCH --exclusive
 
 ## DESCRIPTION: This job downloads and creates and index of the human reference genome.
 
@@ -10,6 +10,7 @@ date
 
 cd /data/zhanglab/zpimentel/ChIP-Seq
 mkdir 02_mapping && cd 02_mapping
+mkdir mapping_out # for mapping output in next array job
 
 module load Bowtie2/2.4.2-GCC-9.3.0
 
@@ -17,7 +18,8 @@ module load Bowtie2/2.4.2-GCC-9.3.0
 wget https://www.encodeproject.org/files/GRCh38_no_alt_analysis_set_GCA_000001405.15/@@download/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta.gz
 gunzip GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta.gz
 
-bowtie2-build --threads 20 GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta GRCh38
+# Index the reference genome with bowtie2
+bowtie2-build --threads $SLURM_NTASKS_PER_NODE GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta GRCh38
 
 echo "DONE"
 date
